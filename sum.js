84da99360ad64a927531cc6ab4a9f67605d60f03
isha-web1 <isha.web1@gmail.com>
2023-02-06 00:45:58 +0600

    simple sum added

---
+++ sum.js
@@ -0,0 +1,15 @@
+/* 1 থেকে n পর্যন্ত সংখ্যার যোগফল নির্ণয় করো। */
+
+let n = 10;
+sum = 0;
+for(let i = 1; i <= n; i++){
+    sum = sum + i;
+}
+console.log(sum);
+
+
+/* another way to this problem solve/*  */ 
+
+
+/* let sum2 = (n*(n+1))/2;
+console.log(sum2); */
