41c7e551252f7dfebada65dcb650dcc1b1a5ce0f
isha-web1 <isha.web1@gmail.com>
2023-02-06 00:52:38 +0600

    multiplication table added

---
+++ multiplication.js
@@ -0,0 +1,9 @@
+/* ৩ এর নামতা লিখো জাভাস্ক্রিপ্ট ব্যাবহার করে। */
+
+
+function multiplicationTable(multiply){
+    for(let i = 1; i<=10; i++){
+        console.log(3 + '*' + i + "=" + i*3);
+    }
+}
+multiplicationTable()
