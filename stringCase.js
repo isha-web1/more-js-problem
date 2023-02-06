f36762ec804f0fd74c0e63eb285b76528fe4e10d
isha-web1 <isha.web1@gmail.com>
2023-02-06 12:12:37 +0600

    stringCase added

---
+++ stringCase.js
@@ -0,0 +1,10 @@
+/* একটা ফাংশন লিখবা যেটা যেকোন নামকে uppercase বা রেগুলার কেইস হিসেবে নিবে আর আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে। */
+
+
+function stringCase(name){
+    const myName = 'Jhon Abraham'.toLowerCase();
+  
+    return myName;
+}
+console.log(stringCase());
+
